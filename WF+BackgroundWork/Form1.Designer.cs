2bfba063f5f9a9909f6d1a182b9007886f0c3d4d
Висковатых Наталия Валентиновна <.@itstep.kharkov.ua>
2019-09-20 10:59:30 +0300

    Backgound

---
+++ WF+BackgroundWork/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace WF_BackgroundWork
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.bckgWorker = new System.ComponentModel.BackgroundWorker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblSum = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(320, 184);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 0;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(25, 184);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(178, 43);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 13);
+            this.lblResult.TabIndex = 2;
+            // 
+            // bckgWorker
+            // 
+            this.bckgWorker.WorkerReportsProgress = true;
+            this.bckgWorker.WorkerSupportsCancellation = true;
+            this.bckgWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bckgWorker_DoWork);
+            this.bckgWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bckgWorker_ProgressChanged);
+            this.bckgWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bckgWorker_RunWorkerCompleted);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(25, 100);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(58, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(164, 100);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(69, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(251, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "=";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(102, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "+";
+            // 
+            // lblSum
+            // 
+            this.lblSum.AutoSize = true;
+            this.lblSum.Location = new System.Drawing.Point(343, 106);
+            this.lblSum.Name = "lblSum";
+            this.lblSum.Size = new System.Drawing.Size(35, 13);
+            this.lblSum.TabIndex = 7;
+            this.lblSum.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 235);
+            this.Controls.Add(this.lblSum);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnStop);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblResult;
+        private System.ComponentModel.BackgroundWorker bckgWorker;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblSum;
+    }
+}
+
---
+++ WF+BackgroundWork/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WF_BackgroundWork
+{
+   
+    public partial class Form1 : Form
+    {
+        public int Summ(int from, int to)
+        {
+            if (from > to)
+            {
+                int temp = from;
+                from = to;
+                to = temp;
+            }
+            int result = from;
+            for(int i = from; i <= to; i++)
+            {
+                result += i;
+            }
+            return result;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+            if (bckgWorker.IsBusy == false)
+            {
+                int from;
+                if(!int.TryParse(textBox1.Text,out from))
+                {
+                    MessageBox.Show("Error first num is doesn't correct");
+                    return;
+                }
+                int to;
+                if (!int.TryParse(textBox2.Text, out to))
+                {
+                    MessageBox.Show("Error first num is doesn't correct");
+                    return;
+                }
+                Examlple ex = new Examlple { From = from, To = to };
+
+                //bckgWorker.RunWorkerAsync();//запускает событие doWork
+                bckgWorker.RunWorkerAsync(ex);//запускает событие doWork
+
+
+                btnStart.Enabled = false;
+            }
+
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (bckgWorker.WorkerSupportsCancellation)
+            {
+                bckgWorker.CancelAsync();
+            }
+        }
+
+        private void bckgWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Examlple examlple = e.Argument as Examlple;
+
+            e.Result = this.Summ(examlple.From, examlple.To);
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (bckgWorker.CancellationPending)
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                //if (i == 50)//принудительно остановили его на 50, и дальше на 100% не дойдет(((
+                //{
+                //    throw new Exception("Test!");
+                //}
+                Thread.Sleep(200);
+                bckgWorker.ReportProgress(i);
+            }
+        }
+
+        private void bckgWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            lblResult.Text = $"{e.ProgressPercentage}%";
+        }
+
+        private void bckgWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            lblSum.Text = e.Result.ToString();
+            if (e.Cancelled)
+            {
+                lblResult.Text = $"Operation is delete";
+            }
+            else if (e.Error!=null)
+            {
+                lblResult.Text = $"Error:{e.Error.Message}";
+            }
+            else
+            {
+                lblResult.Text = $"Every is ok";
+            }
+            btnStart.Enabled = true;
+        }
+    }
+    class Examlple
+    {
+        public int From { get; set; }
+        public int To { get; set; }
+
+    }
+}
